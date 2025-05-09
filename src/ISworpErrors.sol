1f718d0392e95bd3ff343f031944ffecfca189f5
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-05-09 21:54:52 +0100

    refactor

--- src/ISworpErrors.sol
+++ src/ISworpErrors.sol
@@ -2,20 +2,20 @@
 pragma solidity ^0.8.15;
 
 interface ISwapperErrors {
-    error Swapper__NotOwnedByRequester(address _nft, uint256 _id);
-    error Swapper__NotOwnedByRequestee(address _nft, uint256 _id);
+    error Swapper__NotOwnedByRequester(address nft, uint256 id);
+    error Swapper__NotOwnedByRequestee(address nft, uint256 id);
     error Swapper__SelfOrder();
     error Swapper__InvalidAddress();
     error Swapper__NotApproved(address requester);
     error Swapper__BadOrder();
     error Swapper__InvalidFulfiller(address impersonator);
     error Swapper__FulfillerInboxFull(uint8 size);
-    error Swapper__NotAdmin(address _user);
-    error Swapper__InsufficientOrderFee(uint256 _fee);
+    error Swapper__NotAdmin(address user);
+    error Swapper__InsufficientOrderFee(uint256 fee);
 
-    event CreateSwapOrder(address indexed _from, address _to, uint256 _requestId);
-    event CreateSwapOrderMulti(address indexed _from, address _to, uint256 _requestId);
-    event FufillSwapOrder(uint256 _requestId);
-    event RejectSwapOrder(uint256 _requestId);
-    event CancelSwapOrder(uint256 _requestId);
+    event CreateSwapOrder(address indexed from, address to, uint256 orderId);
+    event CreateSwapOrderMulti(address indexed from, address to, uint256 orderId);
+    event FufillSwapOrder(uint256 orderId);
+    event RejectSwapOrder(uint256 orderId);
+    event CancelSwapOrder(uint256 orderId);
 }
