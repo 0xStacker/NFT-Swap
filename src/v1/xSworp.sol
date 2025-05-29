9a243a44ca7fb8f20657788a99aea4b10ffdaba0
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-05-29 08:42:39 +0100

    refactor

--- src/v1/xSworp.sol
+++ src/v1/xSworp.sol
@@ -1,12 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-
 import {IERC721} from "@openzeppelin/token/ERC721/IERC721.sol";
 import {IERC721Receiver} from "@openzeppelin/token/ERC721/IERC721Receiver.sol";
 import {SworpUtils} from "../SworpUtils.sol";
 
-contract xSworp is SworpUtils{
- 
-    
-}
+contract xSworp is SworpUtils {}
