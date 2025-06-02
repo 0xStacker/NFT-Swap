9d5addc4e1accb9c1b1afaf0036b67e67561d004
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-06-02 23:01:31 +0100

    refactor for new architecture

--- src/SworpUtils.sol
+++ src/SworpUtils.sol
@@ -13,9 +13,6 @@ abstract contract SworpUtils is ISworpErrors {
     // Maximum number of nfts that can be included in a single transaction.
     uint8 internal constant MAX_TRADEABLE_NFT = 15;
 
-    // Fees for creating an order involving multiple nfts. 1 to 1 orders are free.
-    uint256 internal constant FEE_PER_NFT = 0.0005 ether;
-
     // Admin address.
     address internal _admin;
 
@@ -27,41 +24,11 @@ abstract contract SworpUtils is ISworpErrors {
 
     // pending pool contains all pending orders.
 
-    Request[] pendingPool;
+    uint256[] public orderPool;
 
-    mapping(uint256 => uint256) _orderIndexTracker
+    mapping(uint256 => uint256) internal _orderIndexTracker;
 
-    uint nextIndexAssigner;
-
-    // General request pool
-    mapping(address => mapping(uint256 => Request)) public _orderPool;
-
-    // Keep track of order index inside user inbox for easier removal after completion.
-    mapping(address => mapping(uint256 => uint256)) public inboxOrderIndexTracker;
-
-    // Keep track of order index inside user outbox for easier removal after completion.
-    mapping(address => mapping(uint256 => uint256)) internal outboxOrderIndexTracker;
-
-    // Assign index to the next request in the inbox
-    mapping(address => uint256) internal inboxNextIndexTracker;
-
-    // Assign index to the next request in the outbox
-    mapping(address => uint256) internal outboxNextIndexTracker;
-
-    // fulfillers' inbox
-    mapping(address => uint256[]) public fulfillerInbox;
-
-    // Requesters' outbox
-    mapping(address => uint256[]) public requesterOutbox;
-
-    // User's accepted requests
-    mapping(address => uint256[]) public userAcceptedRequests;
-
-    // User's rejected requests
-    mapping(address => uint256[]) public userRejectedRequests;
-
-    // User's canceled requests
-    mapping(address => uint256[]) public userCanceledRequests;
+    uint256 nextIndexAssigner;
 
     // User's approved addresses. Only adresses aproved by user can send nft swap requests.
 
@@ -71,12 +38,6 @@ abstract contract SworpUtils is ISworpErrors {
     enum OrderStatus {
         pending,
         completed
-    }
-
-    // Location enum allows us to keep track of where to remove a given order from.
-    enum Location {
-        outbox,
-        inbox
     }
 
     struct Nft {
@@ -144,7 +105,7 @@ abstract contract SworpUtils is ISworpErrors {
     // Modifiers
 
     modifier onlyFulfiller(Request memory _order) {
-        if (_order.fulfiller != address(0) && msg.sender != _order.fulfiller){
+        if (_order.fulfiller != address(0) && msg.sender != _order.fulfiller) {
             revert Swapper__InvalidFulfiller(msg.sender);
         }
         _;
@@ -168,49 +129,20 @@ abstract contract SworpUtils is ISworpErrors {
 
     /**
      * @dev remove a request from inbox or outbox of a user by swapping it with the last item in the array, then popping the last item.
-     * @param _from is the Location to delete from (inbox or outbox).
-     * @param _user is the user's address.
      * @param _orderId is the unique identifier for the request.
      */
     function removeOrder(uint256 _orderId) internal virtual {
         // Locate order index
-        uint orderIndex = orderIndexTracker[_orderId];
+        uint256 orderIndex = _orderIndexTracker[_orderId];
         require(orderIndex != 0, "Item not in outbox");
-            uint256 itemIndex = --orderIndex;
-            uint256 lastItem = orderPool[orderPool.length - 1];
-            // Swap the order with the last item in the array and remove the last item
-            orderPool[itemIndex] = lastItem;
-            orderPool.pop();
-            orderIndexTracker[lastItem] = ++itemIndex;
-            orderIndexTracker[_orderId] = 0;
-            outboxNextIndexTracker[_user]--;
-
-
-        // if (_from == Location.outbox) {
-        //     // Locate order index
-        //     uint256 orderIndex = outboxOrderIndexTracker[_user][_orderId];
-        //     require(orderIndex != 0, "Item not in outbox");
-        //     uint256 itemIndex = --orderIndex;
-        //     uint256 lastItem = requesterOutbox[_user][requesterOutbox[_user].length - 1];
-        //     // Swap the order with the last item in the array and remove the last item
-        //     requesterOutbox[_user][itemIndex] = lastItem;
-        //     requesterOutbox[_user].pop();
-        //     outboxOrderIndexTracker[_user][lastItem] = ++itemIndex;
-        //     outboxOrderIndexTracker[_user][_orderId] = 0;
-        //     outboxNextIndexTracker[_user]--;
-        // } else {
-        //     // Locate order index
-        //     uint256 orderIndex = inboxOrderIndexTracker[_user][_orderId];
-        //     require(orderIndex != 0, "Item not in inbox");
-        //     uint256 itemIndex = --orderIndex;
-        //     uint256 lastItem = fulfillerInbox[_user][fulfillerInbox[_user].length - 1];
-        //     // Swap the order with the last item in the array and remove the last item
-        //     fulfillerInbox[_user][itemIndex] = lastItem;
-        //     fulfillerInbox[_user].pop();
-        //     inboxOrderIndexTracker[_user][lastItem] = ++itemIndex;
-        //     inboxOrderIndexTracker[_user][_orderId] = 0;
-        //     inboxNextIndexTracker[_user]--;
-        // }
+        uint256 itemIndex = --orderIndex;
+        uint256 lastItem = orderPool[orderPool.length - 1];
+        // Swap the order with the last item in the array and remove the last item
+        orderPool[itemIndex] = lastItem;
+        orderPool.pop();
+        _orderIndexTracker[lastItem] = ++itemIndex;
+        _orderIndexTracker[_orderId] = 0;
+        nextIndexAssigner--;
     }
 
     function setAdmin(address _newAdmin) external virtual onlyAdmin {
@@ -248,35 +180,14 @@ abstract contract SworpUtils is ISworpErrors {
         return IERC721Receiver.onERC721Received.selector;
     }
 
-    // Getter for user inbox.
-    function fetchOrderInbox(address _user) external view returns (uint256[] memory) {
-        return fulfillerInbox[_user];
-    }
-
-    // Getter for user outbox.
-    function fetchOrderOutbox(address _user) external view returns (uint256[] memory) {
-        return requesterOutbox[_user];
-    }
-
-    // Getter for user accepted requests
-    function fetchAcceptedOrders(address _user) external view returns (uint256[] memory) {
-        return userAcceptedRequests[_user];
-    }
-
-    // Getter for user rejected requests
-    function fetchRejectedOrders(address _user) external view returns (uint256[] memory) {
-        return userRejectedRequests[_user];
-    }
-
-    // Getter for user canceled requests.
-    function fetchCanceledOrders(address _user) external view returns (uint256[] memory) {
-        return userCanceledRequests[_user];
+    function fetchPendingOrders() external view returns (uint256[] memory) {
+        return orderPool;
     }
 
     /**
      * @dev Get the order details.
      */
     function getOrder(uint256 _orderId) public view returns (Request memory) {
-        return _orderPool[msg.sender][_orderId];
+        return orderMarket[_orderId];
     }
 }
