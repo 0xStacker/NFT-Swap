7bde829ea0c2acfdba82617a71516ad47556a7e7
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-06-02 23:03:59 +0100

    refactor for testing of new architecture

--- test/testNft.sol
+++ test/testNft.sol
@@ -5,10 +5,11 @@ pragma solidity ^0.8.6;
 import {ERC721} from "@openzeppelin/token/ERC721/ERC721.sol";
 
 contract Nft is ERC721("NFT", "NFT") {
-    uint256 tokenId = 1;
+    uint256 tokenId;
 
-    function mint() external {
-        _safeMint(msg.sender, tokenId);
+    function mint() external returns (uint256) {
         tokenId += 1;
+        _safeMint(msg.sender, tokenId);
+        return tokenId;
     }
 }
