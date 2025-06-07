34c4d7831c945975b7de867d7c68c436a81f69ec
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-06-07 21:52:56 +0100

    refactor

--- src/ISworpErrors.sol
+++ src/ISworpErrors.sol
@@ -8,19 +8,17 @@ interface ISworpErrors {
     error Swapper__InvalidAddress();
     error Swapper__NotApproved(address requester);
     error Swapper__BadOrder();
+    error Swapper__ERC721LimitExceeded(uint limit);
     error Swapper__BadOrderMatch();
-    error Swapper__InvalidFulfiller(address impersonator);
-    error Swapper__FulfillerInboxFull(uint8 size);
+    error Swapper__InvalidMatcher(address matcher);
     error Swapper__NotAdmin(address user);
-    error Swapper__InsufficientOrderFee(uint256 fee);
+    error Swapper__InsufficientFunds(uint256 funds);
     error Swapper__InsufficientAllowance(address token, uint256 amount);
     error Swapper__InsufficientTokenBalance(address token, uint256 amount);
     error Swapper__EthTransferFailed(address to, uint256 amount);
     error Swapper__ERC20TransferFailed(address token, address to, uint256 amount);
 
-    event CreateSwapOrder(address indexed from, address to, uint256 orderId);
-    event CreateSwapOrderMulti(address indexed from, address to, uint256 orderId);
-    event FufillSwapOrder(uint256 orderId);
-    event RejectSwapOrder(uint256 orderId);
-    event CancelSwapOrder(uint256 orderId);
+    event CreateSwapOrder(address indexed creator, uint256 orderId);
+    event MatchSwapOrder(address matcher, uint256 orderId);
+    event CancelSwapOrder(address creator, uint256 orderId);
 }
